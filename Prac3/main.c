8435a05281d633d1e94363bc5cc5bab268e99554
Prince Mashava <151672318+mashavaprince09@users.noreply.github.com>
2024-09-18 16:13:03 +0200

    Initialised a string to output second line on LCD
    Changed LED pattern; output 0x01 if the read SPI data is incorrect

--- Prac3/main.c
+++ Prac3/main.c
@@ -487,9 +487,26 @@ void TIM16_IRQHandler(void)
 	HAL_TIM_IRQHandler(&htim16);
 
 	// TODO: Initialise a string to output second line on LCD
-
-
 	// TODO: Change LED pattern; output 0x01 if the read SPI data is incorrect
+    uint8_t eepromValue = read_from_address(currentAddress);
+
+    // Check if the read value matches the expected value from binaryArray
+    if (eepromValue == binaryArray[currentAddress])
+    {
+        // Format and print the correct value to LCD
+        char lcdLine[16];
+        //f(lcdLine, sizeof(lcdLine), "EEPROM byte:\n%d", eepromValue);
+        sprintf(lcdLine, "%d", eepromValue);
+        writeLCD(lcdLine);
+    }
+    else
+    {
+        // Print SPI error message to LCD
+        writeLCD("SPI ERROR!");
+    }
+
+    // Update the current address and wrap around if needed
+    currentAddress = (currentAddress + 1) % 6;
 	
   
 
